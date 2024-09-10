ecd939b8606e9da275e1d2e7af0e86335f51843b
karasicevic <ak20190190@student.fon.bg.ac.rs>
2024-09-10 14:16:41 +0200

    changed assembly info

--- MasterTPSApp.Server/obj/Debug/net8.0/MasterTPSApp.Server.AssemblyInfo.cs
+++ MasterTPSApp.Server/obj/Debug/net8.0/MasterTPSApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterTPSApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53194fa6338011b911fe0dc31868506035ca8047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c20c755171a49ad4a0902ff87bbe6e6a7a7c98b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterTPSApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterTPSApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
